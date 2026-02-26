80144abe61fd2f2828f67f7b3ba38107f9cfbcbb
apachertr180navneeet <navneet@brenin.co>
2026-02-26 12:49:48 +0530

    intinal commit

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layouts.app')
+@section('style')
+@endsection  
+
+@section('content')
+
+                    <!-- Content -->
+
+                    <div class="container-fluid flex-grow-1 container-p-y">
+                        <div class="row">
+                            <div class="col-lg-8 mb-4 order-0">
+                                <div class="card">
+                                    <div class="d-flex align-items-end row">
+                                        <div class="col-sm-7">
+                                            <div class="card-body">
+                                                <h5 class="card-title text-primary">Congratulations {{ auth()->user()->full_name }}! 🎉</h5>
+                                                <p class="mb-4">
+                                                    You have done <span class="fw-medium">72%</span> more sales today.
+                                                    Check your new badge in
+                                                    your profile.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- / Content -->
+
+                    <!-- Footer -->
+                    
+                    <!-- / Footer -->
+
+                   
+@endsection
+
+@section('script')
+@endsection
